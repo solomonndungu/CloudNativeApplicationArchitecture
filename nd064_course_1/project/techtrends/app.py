de1f9582853c8cb0ba2ce7e6eb33c65022714006
solomonndungu <ssouljabee@gmail.com>
2021-09-17 17:50:51 +0300

    added the main folder

---
+++ nd064_course_1/project/techtrends/app.py
@@ -0,0 +1,133 @@
+import sqlite3
+import logging
+import sys
+
+from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
+from werkzeug.exceptions import abort
+
+
+# Variable for counter
+conn_counter = 0
+# Function to get a database connection.
+# This function connects to database with the name `database.db`
+def get_db_connection():
+    # global variable that will increment each time a connection to the database is made
+    global conn_counter
+
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row
+    # counting by increment per every database connection
+    conn_counter += 1
+    return connection
+
+# Function to get a post using its ID
+def get_post(post_id):
+    connection = get_db_connection()
+    post = connection.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    connection.close()
+    return post
+
+# Define the Flask application
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+
+# Define the main route of the web application 
+@app.route('/')
+def index():
+    connection = get_db_connection()
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+    return render_template('index.html', posts=posts)
+
+# Healthcheck endpoint for the TechTrends application
+@app.route('/healthz')
+def healthcheck():
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status = 200,
+        mimetype='application/json'
+    )
+    return response
+
+# Metrics endpoint
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()   
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+
+    response = app.response_class(
+        response=json.dumps({"status":"success","code":0,"data":{"post_count":len(posts), "db_connection_count":conn_counter}}),
+        status=200,
+        mimetype='application/json'
+    )
+    
+    return response
+
+
+# Define how each individual article is rendered 
+# If the post ID is not found a 404 page is shown
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    if post is None:
+        app.logger.info('The article does not exist')
+        return render_template('404.html'), 404
+    else:
+
+      return render_template('post.html', post=post)
+
+# Define the About Us page
+@app.route('/about')
+def about():
+    # About us page is retrieved.
+    app.logger.info('About us page request successfull')
+
+    return render_template('about.html')
+
+# Define the post creation functionality 
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        # The title of existing article is recorded in the logline.
+        app.logger.info('Article %s Retrieved!', title)
+
+        if not title:
+            flash('Title is required!')
+        else:
+            connection = get_db_connection()
+            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            connection.commit()
+            connection.close()
+
+            # Title of new article is recorded in the logline
+            app.logger.info('Article %s Retrieved!', title)
+
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+# start the application on port 3111
+if __name__ == "__main__":
+
+    # Create formatter to apply to handlers
+    format_output = ('%(asctime)s: %(levelname)s: %(name)s: %(message)s')
+
+    # Set logger to handle STDOUT and STDERR
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    stdout_handler.setLevel(logging.DEBUG)
+
+    stderr_handler = logging.StreamHandler(sys.stderr)
+    stderr_handler.setLevel(logging.DEBUG)
+
+    handlers = [stdout_handler, stderr_handler]
+
+    ## stream logs to app.log file
+    # logging.basicConfig(filename='app.log', format=format_output, level=logging.DEBUG, handlers=handlers)
+    logging.basicConfig(level=logging.DEBUG, format=format_output, handlers=handlers)
+
+    app.run(host='0.0.0.0', port='3111')
